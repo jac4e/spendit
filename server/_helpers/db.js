06f2693c7760f8368f0bee39f7920ad90455e55d
Jacques Fourie <jacques@fourie.ca>
2022-03-15 07:16:56 -0600

    no user for db for now

--- server/_helpers/db.js
+++ server/_helpers/db.js
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 import config from '../config.js';
-mongoose.connect(`mongodb://{config.db.user}:{config.db.password}@{config.db.url}:{config.db.port}/{config.db.name}`);
+mongoose.connect(`mongodb://${config.db.url}:${config.db.port}/${config.db.name}`);
 mongoose.Promise = global.Promise;
 
 import user from '../_models/account.model.js';

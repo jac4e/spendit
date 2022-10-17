45df8fd7e921759e5dd42bf606cd3b7a8ebf8b8a
Jacques Fourie <jacques@fourie.ca>
2022-10-17 11:43:00 -0600

    removed server

--- server/_helpers/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import config from '../deploy/config.js';
-mongoose.connect(`mongodb://${config.db.url}:${config.db.port}/${config.db.name}`);
-
-import account from '../_models/account.model.js';
-import product from '../_models/product.model.js';
-import transaction from '../_models/transaction.model.js';
-
-export default { account, product, transaction };

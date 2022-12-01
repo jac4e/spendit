598807505ccd7a92a8a33ed2461936fe0a2c483d
Jacques Fourie <jacques@fourie.ca>
2022-12-01 00:25:22 -0700

    Started mockup

--- client/app/account/settings/settings.component.ts
+++ client/app/account/settings/settings.component.ts
@@ -1,4 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup,
+  UntypedFormBuilder,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
+import { first } from 'rxjs';
+import { IAccount } from 'typesit';
+import { AccountService, AlertService } from '../../_services';
+
+class SettingsForm {
+  form: FormGroup;
+  showErrors: { [key: string]: boolean };
+  loading: boolean;
+  controlsConfig: { [key: string]: any };
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    controlsConfig: { [key: string]: any }
+  ) {
+    this.controlsConfig = controlsConfig;
+    this.showErrors = {};
+    Object.keys(controlsConfig).map((key) => {
+      this.showErrors[key] = false; 
+    })
+    this.loading = false;
+    this.form = this.formBuilder.group(this.controlsConfig);
+  }
+}
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +35,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.sass']
 })
 export class SettingsComponent implements OnInit {
+  account = {} as IAccount;
+  usernameForm!: SettingsForm;
+  nameForm!: SettingsForm;
+  emailForm!: SettingsForm;
+  notificationForm!: SettingsForm;
+  passwordForm!: SettingsForm;
 
-  constructor() { }
+  constructor(
+    private formBuilder: UntypedFormBuilder,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) {
+    this.accountService.account.subscribe((account) => {
+      if (account !== null) {
+        this.account = account;
+        this.usernameForm = new SettingsForm(formBuilder, {
+          username: [this.account.username, [Validators.required]]
+        });
+        this.nameForm = new SettingsForm(formBuilder, {
+          firstName: [this.account.firstName, [Validators.required]],
+          lastName: [this.account.lastName, [Validators.required]]
+        });
+        this.emailForm = new SettingsForm(formBuilder, {
+          email: [
+            this.account.email,
+            [
+              Validators.required,
+              Validators.email,
+              Validators.pattern(/@ualberta.ca$/)
+            ]
+          ]
+        });
+        this.notificationForm = new SettingsForm(formBuilder, {
+          notify: [this.account.notify]
+        });
+        this.passwordForm = new SettingsForm(formBuilder, {
+          password: ['', [Validators.required, this.passwordValidator]],
+          confirmPassword: ['', [Validators.required, this.passwordValidator]],
+        });
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onFocusOut(form: SettingsForm, key: string) {
+    // this.showErrors[key] = true;
+  }
+
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    // Get fields
+    const password = control.parent?.get('password');
+    const confirmPassword = control.parent?.get('confirmPassword');
+
+    // Check if password and confirm password exist
+    if (!password || !confirmPassword) {
+      return null;
+    }
+
+    // If there are errors on password but not passwordMatch error,
+    // let user fix those first before displying confirmPassword error
+    if (password.errors !== null && !password.hasError('passwordMatch')) {
+      return null;
+    }
+
+    // Do not check for matching passwords if passwords are empty
+    if (password.value === '' || confirmPassword.value === '') {
+      return null;
+    }
+
+    // Check if password matches confirmPassword
+    if (password.value !== confirmPassword.value) {
+      password.setErrors({ passwordMatch: true });
+      confirmPassword.setErrors({ passwordMatch: true });
+      return { passwordMatch: true };
+    } else {
+      password.setErrors(null);
+      confirmPassword.setErrors(null);
+      return null;
+    }
+  }
 }

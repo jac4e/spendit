7a217a078ea17842af28681b750fe083160f6a33
Jacques Fourie <jacques@fourie.ca>
2024-12-12 13:46:42 -0700

    Updated refill template to look better and show surcharge amount

--- client/app/account/refill/refill.component.ts
+++ client/app/account/refill/refill.component.ts
@@ -201,7 +201,7 @@ export class RefillComponent {
     // Submit the refill
     this.accountService.requestRefill(
       this.refillForm.value["method"],
-      this.refillForm.value["amount"]
+      String(this.refillForm.value["amount"])
     ).subscribe({
       next: (refill) => {
         this.refillStep = this.refillForm.value["method"];
@@ -223,34 +223,6 @@ export class RefillComponent {
     return this.accountService.getRefillHistory()
   }
 
-  // test stripe stuff below
-  @ViewChild(StripePaymentElementComponent) paymentElement!: StripePaymentElementComponent;
-  private readonly fb = inject(UntypedFormBuilder);
-  paymentElementForm = this.fb.group({
-    name: ['John Doe', [Validators.required]],
-    email: ['support@ngx-stripe.dev', [Validators.required]],
-    address: [''],
-    zipcode: [''],
-    city: [''],
-    amount: [2500, [Validators.required, Validators.pattern(/\d+/)]]
-  });
-
-  elementsOptions: StripeElementsOptions = {
-    mode: 'payment',
-    amount: 1099,
-    currency: 'usd',
-    locale: 'es'
-  };
-
-  paymentElementOptions: StripePaymentElementOptions = {
-    layout: {
-      type: 'tabs',
-      defaultCollapsed: false,
-      radios: false,
-      spacedAccordionItems: false
-    }
-  };
-
   // Replace with your own public key
   paying = false;
 
@@ -266,38 +238,25 @@ export class RefillComponent {
         });
       }
     });
-
-    // this.stripe.redirectToCheckout
-    // this.stripe
-    //   .confirmPayment({
-    //     elements: this.paymentElement.elements,
-    //     confirmParams: {
-    //       payment_method_data: {
-    //         // billing_details: {
-    //         //   name: name as string,
-    //         //   email: email as string,
-    //         //   address: {
-    //         //     line1: address as string,
-    //         //     postal_code: zipcode as string,
-    //         //     city: city as string
-    //         //   }
-    //         // }
-    //       }
-    //     },
-    //     redirect: 'if_required'
-    //   })
-    //   .subscribe(result => {
-    //     this.paying = false;
-    //     if (result.error) {
-    //       // Show error to your customer (e.g., insufficient funds)
-    //       alert({ success: false, error: result.error.message });
-    //     } else {
-    //       // The payment has been processed!
-    //       if (result.paymentIntent.status === 'succeeded') {
-    //         // Show a success message to your customer
-    //         alert({ success: true });
-    //       }
-    //     }
-    //   });
   }
+
+  getFormAmount(method: string) {
+    return (Number(this.refillForm.get('amount')?.value) || 0)/100;
+  }
+
+  getSurcharge(method: string) {
+    const amount = Number(this.refillForm.get('amount')?.value) || 0;
+
+    if (amount===0) return 0;
+
+    switch (method) {
+      case RefillMethods.Stripe:
+        return (Math.round((amount + 30) / (1 - 0.029)) - amount)/100;
+      case RefillMethods.Card:
+        return (Math.round((amount + 30) / (1 - 0.027)) - amount)/100;
+      default:
+        return 0;
+    }
+  }
+
 }
